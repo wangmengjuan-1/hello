1570751474da963f29206ef7869b9ee75db91f9f
e <842292812@qq.com>
2021-03-07 20:17:11 +0800

    练习class和style

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-html="`<span>1</span>`"></div>
+    <div v-text="`<span>1</span>`"></div>
+    <div>{{firstName}}</div>
+    <div>{{lastName}}</div>
+    <div>{{fullName}}</div>
+    <div :class="{blue:isBlue,red:isRed}">王孟娟</div>
+    <div :style="{color:isBlue? 'blue': 'red'}">王孟娟2</div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+       firstName: 'first',
+       lastName: 'last',
+       isBlue:true,
+       isRed: false
+    }
+  },
+  computed: {
+    fullName() {
+      return this.firstName + ' ' + this.lastName
+    }
+  },
+  mounted(){
+    setInterval(() => {
+      this.isBlue = !this.isBlue;
+      this.isRed = !this.isRed;
+    }, 3000)
   }
 }
 </script>
@@ -24,5 +48,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.blue {
+  color: blue;
+}
+.red {
+  color:red;
 }
 </style>
